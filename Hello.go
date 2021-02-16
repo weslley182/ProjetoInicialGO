f6b080ff71bd30e447da65f11f3bc4c150ceae0c
Wesley Machado <weslley182@gmail.com>
2021-02-16 18:44:33 -0300

    utilizando funçoes

--- Hello.go
+++ Hello.go
@@ -1,21 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	exibeIntroducao()
+
+	exibeMenu()
+
+	comando := leComando()
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println(("Logs..."))
+	case 0:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	default:
+		fmt.Println("Erro...")
+		os.Exit(-1)
+	}
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do programa")
+	fmt.Println()
+}
+
+func exibeIntroducao() {
 	nome := "Wesley"
 	versao := 1.1
 
 	fmt.Println("Bom dia", nome)
 	fmt.Println("Versão", versao)
+	fmt.Println()
+}
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do programa")
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
 
-	var comando int
-	fmt.Scan(&comando)
+	fmt.Println("Valor da variável: ", comandoLido)
+	fmt.Println("Endereço da variável:", &comandoLido)
+	fmt.Println()
 
-	fmt.Println("Valor da variável: ", comando)
-	fmt.Println("Endereço da variável:", &comando)
+	return comandoLido
 }
